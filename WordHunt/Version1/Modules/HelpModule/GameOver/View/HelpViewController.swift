04fce342236ee43aac2226f37778aa5b262e6a39
AdityaNeoK <adityakhavanekar98@gmail.com>
2023-05-21 19:49:31 +0530

    Modified helpViewController

--- WordHunt/Version1/Modules/HelpModule/GameOver/View/HelpViewController.swift
+++ WordHunt/Version1/Modules/HelpModule/GameOver/View/HelpViewController.swift
@@ -9,10 +9,16 @@ import UIKit
 
 class HelpViewController: UIViewController {
     
+    @IBOutlet weak var internalViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var btnView: UIView!
     @IBOutlet weak var internalView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if UIScreen.main.bounds.height <= 850{
+            internalViewHeightConstraint.constant = internalViewHeightConstraint.constant + 70
+        }
+        btnView.layer.cornerRadius = 15
         internalView.layer.cornerRadius = 20
     }
 
