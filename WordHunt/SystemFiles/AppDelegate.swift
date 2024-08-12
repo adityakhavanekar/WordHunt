333d104990b8cc51442c1a26c06d9fc459f356b3
Aditya Khavanekar <adityakhavanekar98@gmail.com>
2024-08-11 21:07:09 -0400

    Removed GOOGLE ADS

--- WordHunt/SystemFiles/AppDelegate.swift
+++ WordHunt/SystemFiles/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GADMobileAds.sharedInstance().start(completionHandler: nil)
+//        GADMobileAds.sharedInstance().start(completionHandler: nil)
 //        GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers =
 //                    [ "6895abf8d6a764722bc25d81f1b3f068" ]
         return true
